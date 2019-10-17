e95dff84ccbf40d48662f4108c6a6a3916567ebe
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-10-17 13:36:28 -0700

    add private non-functional extension point to show package in version-check (#2)

--- colcon_cd/__init__.py
+++ colcon_cd/__init__.py
@@ -2,3 +2,9 @@
 # Licensed under the Apache License, Version 2.0
 
 __version__ = '0.0.0'
+
+
+class NonFunctionalExtensionPoint:
+    """Non-functional extension point to list the package in version-check."""
+
+    pass
