colcon/colcon-cd

commit 78e4976f903a5a09344f609c8b57e666a0515e81

Add colcon_cd bash completer (#14)
Signed-off-by: Chen Bainian <chenbn@artc.a-star.edu.sg>

function/colcon_cd.sh
@@ -1,7 +1,9 @@
 # copied from colcon-cd/function/colcon_cd.sh
 
 colcon_cd() {
-  if [ $# = 0 ]; then
+  if [ "$_ARGCOMPLETE" == "1" ]; then
+    python3 -m colcon_cd.completer
+  elif [ $# = 0 ]; then
     # change the working directory to the previously saved path
     if [ "$_colcon_cd_root" = "" ]; then
       echo "No previous path saved. Either call 'colcon_cd <pkgname>' from a" \
@@ -14,7 +16,7 @@ colcon_cd() {
     fi
 
   elif [ $# = 1 ]; then
-    if [ "$1" = "--help" ]; then
+    if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
       echo "usage: colcon_cd [--set] [--reset] [PACKAGE_NAME]"
       echo ""
       echo "Change the current working directory."

setup.cfg
@@ -32,6 +32,8 @@ packages = find:
 zip_safe = true
 
 [options.extras_require]
+completion = 
+  colcon-argcomplete
 test =
   flake8>=3.6.0
   flake8-blind-except
@@ -52,6 +54,8 @@ test =
 # distutils replaces dashes in keys with underscores
 share/colcon_cd/function =
     function/colcon_cd.sh
+    function/colcon_cd-argcomplete.bash
+    function/colcon_cd-argcomplete.zsh
 
 [tool:pytest]
 filterwarnings =

colcon_cd/completer.py
@@ -1,0 +1,39 @@
+# Copyright 2021 Chen Bainian
+# Licensed under the Apache License, Version 2.0
+
+import argparse
+
+import argcomplete
+from colcon_argcomplete.argument_parser.argcomplete.completer.package_name \
+    import package_name_completer
+
+
+def main():
+    """
+    Fake command with parser for colcon_cd completer.
+
+    Using package_name_completer to construct
+    a bash completer for the colcon_cd command.
+    """
+    parser = argparse.ArgumentParser()
+
+    # base_path argument is needed for package_name_completer
+    parser.add_argument(
+        '--base-paths',
+        default='.')
+
+    # Add --set and --reset argument for the completer
+    parser.add_argument('--set')
+
+    parser.add_argument('--reset')
+
+    # Add package name completer
+    parser.add_argument(
+        'package_name').completer = package_name_completer
+
+    argcomplete.autocomplete(parser)
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()

function/colcon_cd-argcomplete.bash
@@ -1,0 +1,5 @@
+if type register-python-argcomplete3 > /dev/null 2>&1; then
+  eval "$(register-python-argcomplete3 colcon_cd)"
+elif type register-python-argcomplete > /dev/null 2>&1; then
+  eval "$(register-python-argcomplete colcon_cd)"
+fi

function/colcon_cd-argcomplete.zsh
@@ -1,0 +1,7 @@
+autoload -U +X compinit && compinit
+autoload -U +X bashcompinit && bashcompinit
+if type register-python-argcomplete3 > /dev/null 2>&1; then
+  eval "$(register-python-argcomplete3 colcon_cd)"
+elif type register-python-argcomplete > /dev/null 2>&1; then
+  eval "$(register-python-argcomplete colcon_cd)"
+fi

test/spell_check.words
@@ -1,4 +1,8 @@
 apache
+argcomplete
+argparse
+autocomplete
+bainian
 colcon
 iterdir
 linter

commit 26ef6ebb2a31990ca8cc7bba19ce45fe121072f1

Fix regression in non-Bash shells
Also fix some trailing whitespace that came from the same change.

Fixes: 78e4976f903a5a09344f609c8b57e666a0515e81

function/colcon_cd.sh
@@ -1,7 +1,7 @@
 # copied from colcon-cd/function/colcon_cd.sh
 
 colcon_cd() {
-  if [ "$_ARGCOMPLETE" == "1" ]; then
+  if [ "$_ARGCOMPLETE" = "1" ]; then
     python3 -m colcon_cd.completer
   elif [ $# = 0 ]; then
     # change the working directory to the previously saved path

setup.cfg
@@ -32,7 +32,7 @@ packages = find:
 zip_safe = true
 
 [options.extras_require]
-completion = 
+completion =
   colcon-argcomplete
 test =
   flake8>=3.6.0